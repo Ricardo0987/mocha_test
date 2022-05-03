c18805fd2a2f3109cb25d5100fcfe648e5fa44fe
ricardoqr123 <ricardo.ospina@qrvey.com>
2022-05-03 16:28:38 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,42 @@
-var assert = require('assert');
+// var assert = require("assert");
+const pactum = require("pactum");
+const CFonts = require("cfonts");
 
 const BASE_URL = !process.env.BASE_URL && "http://rickandmortyapi.com";
 const character = !process.env.character && 521;
 
-describe("rtest group", function () {
-  describe("test 1", function () {
-    it("get all characters", function () {
-      assert.equal(5, 5);
+describe("rickandmortyapi API test", function () {
+  describe("characters test", function () {
+    it("get all characters", async () => {
+      await pactum
+        .spec()
+        .get(`${BASE_URL}/api/character/`)
+        .expectStatus(200)
+        .expectJsonLike({
+          info: { count: 826, pages: 42 },
+        });
+    });
+    it("get specific character", async () => {
+      await pactum
+        .spec()
+        .get(`${BASE_URL}/api/character/${character}`)
+        .expectStatus(200)
+        .expectJsonLike({
+          id: 521,
+          name: "Wasp Beth",
+          status: "Alive",
+          species: "Animal",
+          type: "Wasp",
+          gender: "Female",
+        });
     });
   });
 });
 
+CFonts.say("process.env|--------------", {
+  font: "tiny",
+});
 console.log(process.env);
+CFonts.say("--------------", {
+  font: "tiny",
+});
